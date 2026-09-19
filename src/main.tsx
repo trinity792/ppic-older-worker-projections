f247d0e3e1fe520cf5fcfa6ed1357235b5c4a97a
Trinity Jones <trinity_jones@berkeley.edu>
2026-09-18 19:04:51 -0700

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 
 import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
@@ -18,5 +19,6 @@ createRoot(rootElement).render(
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
+    <Analytics />
   </StrictMode>,
 );
